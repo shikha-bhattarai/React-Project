18733476cbd3b5a602d76bf34da2934ecbfafba0
Shikha <sbhatt10@uncc.edu>
2019-11-14 18:52:57 -0500

    Material-UI

--- app/index.js
+++ app/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
  
 class HelloWorld extends React.Component {
     render() {
           return (
-                  <div>
-                    Hello, React!
-                  </div>
-                )
-        }
+            <Button variant="contained" color="primary">
+              Hello World
+              </Button>
+          )
+    }
 };
- 
+
 ReactDOM.render(<HelloWorld />, document.getElementById('root'));
